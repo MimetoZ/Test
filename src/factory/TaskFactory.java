958893aaa9e4bc814705486c4819f3056ab0baf8
MimetaZ <mimizaimova@gmail.com>
2025-05-28 12:48:25 +0300

    Commit

--- src/factory/TaskFactory.java
+++ src/factory/TaskFactory.java
@@ -2,11 +2,11 @@ package factory;
 
 public class TaskFactory {
     public static Task createTask(String typeOfStudent) {
-        switch (typeOfStudent) {
-            case "вундеркинд": return new FunTask();
-            case "фитнес маниак": return new HardTask();
-            case "калитко": return new EasyTask();
-            default: throw new IllegalArgumentException("Неизвестен тип студент.");
-        }
+        return switch (typeOfStudent) {
+            case "вундеркинд" -> new FunTask();
+            case "фитнес маниак" -> new HardTask();
+            case "калитко" -> new EasyTask();
+            default -> throw new IllegalArgumentException("Неизвестен тип студент.");
+        };
     }
 }
