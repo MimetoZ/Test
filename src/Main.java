5b68c13cc4265c383d82b6da76196dc8723d1a04
MimetaZ <mimizaimova@gmail.com>
2025-05-28 13:29:21 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         UniversitySingleton university = UniversitySingleton.getInstance();
 
-        // Студент 1: вундеркинд със суперсила и пристигане
         ArrivalStrategy arrival1 = new RunningAndJumping();
         Student student1 = new SuperFocus(new StudentStandart("вундеркинд", arrival1));
         student1.arrival();
@@ -14,7 +13,6 @@ public class Main {
 
         System.out.println();
 
-        // Студент 2: фитнес маниак със суперсили и пристигане
         ArrivalStrategy arrival2 = new Teleportation();
         Student student2 = new FastWriting(new Telepathy(new StudentStandart("фитнес маниак", arrival2)));
         student2.arrival();
@@ -22,7 +20,6 @@ public class Main {
 
         System.out.println();
 
-        // Студент 3: калитко без суперсили, пристига след спане в автобус
         ArrivalStrategy arrival3 = new SleepOnBus();
         Student student3 = new StudentStandart("калитко", arrival3);
         student3.arrival();
